4c8050f701c651bdd4d051813ab97012d5d3f19d
161250020 <799544882@qq.com>
2020-03-10 18:08:32 +0800

    交互暂存

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ app = Flask(__name__) # 确定APP的启动路径
 title, from_email, to_email, splits, doc, file_list = loadData()
 cluster = {}
 cluster_topics = {}
+doc_keyWords = []
+summary=[]
 '''
 sklearn自带分词：
 '''
@@ -148,7 +150,103 @@ def topics():
             cluster_topics[clu[0]] = tmp
         print("cluster_topics:", cluster_topics)
     print("value:",cluster_topics[int(cate)])
-    return render_template('Topics.html',value=cluster_topics[int(cate)])
+    return render_template('Topics.html',c=cate, value=cluster_topics[int(cate)])
+
+@app.route('/docs')
+def docs():
+    cate=request.args.get('cate')
+    topic=request.args.get('topic')
+    # print("cate:",cate)
+    # print("topic:",topic)
+    '''
+    计算文章的关键词（存特征值的index）
+    取该文章词类型数量的根号作为关键词数量
+    '''
+    if(len(doc_keyWords)==0):
+        for i in range(len(weight)):
+            list_word = weight[i]
+            word_clu_num = 0  # 关键词数量
+            for j in range(len(list_word)):
+                if list_word[j] != 0:
+                    word_clu_num += 1
+            word_clu_num = int(math.sqrt(word_clu_num))
+            distIndexArr = np.argsort(weight[i])
+            topN_index = distIndexArr[:-(word_clu_num + 1):-1]  # top n 的indexs
+            topN_words = np.asarray(word)[topN_index]  # top n 的words---关键词
+            # print("topic_word:",topN_words)
+            topN_words2 = [l for l in topN_words]
+            topN_words2 = topN_words2 + title[i].split(' ')
+            # print("title[i].split:",title[i].split(' '))
+            doc_keyWords.append(topN_words2)
+        print("doc_keyWords:", np.asarray(doc_keyWords))
+
+    '''
+    对文章进行摘要
+    '''
+    if(len(summary)==0):
+        threshold_value = 5  # 门槛值：5
+        for i in range(len(file_list)):  # 对于每一个文章而言
+            # 该篇文章对应的关键词
+            key_words = doc_keyWords[i]
+            # 该文章对应的三个属性
+            sub_split = splits[i]
+            sub_doc = doc[i]
+            sub_file = file_list[i]
+            cluster_value = []  # 存储每一句话的簇的重要性
+            for j in range(len(sub_split)):  # 对于该文章的每一句话
+                # 计算该句子的簇的重要性
+                v = 0  # 初始化该句子重要性
+                split_words = sub_split[j].split(' ')
+                for k in range(len(split_words)):  # 对于每一句话的每个单词
+                    if split_words[k] in key_words:  # 第一个单词为关键词
+                        key_word_num = 1  # 该句的关键词个数
+                        cluster_len = threshold_value + 2  # 簇的长度
+                        last_key_word = False
+                        # 往后数门槛值+1个单词
+                        tmp_split_words = split_words[k + 1:k + 2 + threshold_value]
+                        tmp_split_words_len = len(tmp_split_words)
+                        for w in range(0, tmp_split_words_len):
+                            last_word = tmp_split_words.pop()
+                            if last_word in key_words:
+                                last_key_word = True
+                                key_word_num += 1
+                            if not last_key_word:
+                                cluster_len -= 1
+                        v = max(v, key_word_num * key_word_num / cluster_len)
+                cluster_value.append(v)
+            # print("cluster_value:",cluster_value)
+            # 选出簇的重要性排名靠前的作为该篇文章的摘要
+            n = int(math.sqrt(len(sub_split)))  # 摘要的句子的个数：开根号句子的长度
+            # print("n:",n)
+            distIndexArr = np.argsort(cluster_value)
+            topN_index = distIndexArr[:-(n + 1):-1]
+            topN_index.sort()  # 摘要的句子按照句子在原文中的顺序进行摘要
+            # print("topN_index:",topN_index)
+            sub_summary = ''  # 该篇文章的摘要
+            for j in topN_index:
+                sub_summary = sub_summary + sub_doc[j]
+                # print("sub_summary:",sub_summary)
+            summary.append(sub_summary)
+        print("summary:",summary)
+
+    # 进行响应值的计算
+    docs=cluster_topics[int(cate)][int(topic)][2]
+    print("docs:",docs)
+    from_list=[]
+    to_list=[]
+    title_list=[]
+    key_list=[]
+    summary_list=[]
+    doc_list=[]
+    for i in docs:
+        from_list.append(from_email[i])
+        to_list.append(to_email[i])
+        title_list.append(title[i])
+        key_list.append(doc_keyWords[i])
+        summary_list.append(summary[i])
+        doc_list.append(file_list[i])
+    return render_template('docs.html',cate=cate,topic=topic,len=len(docs),from_list=from_list,to_list=to_list,title_list=title_list,\
+                           key_list=key_list,summary_list=summary_list,doc_list=doc_list)
 
 '''
 @app.route('/g',methods=['GET','POST'])
