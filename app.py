7aee42807741b070740a7d5bcff7eaffdde4badf
聂文韬 <1351446867@qq.com>
2020-04-23 18:12:49 +0800

    添加前端调用借口的尝试

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, jsonify, request
-from Enterprise.controller.enterprise import enterprise
-from flask import Flask, render_template
 from Enterprise.controller.classify_and_relationship import enterprise
 from Enterprise.controller.topics import topics_con
 from Enterprise.controller.summary_and_keywords import summary_con
@@ -28,9 +26,6 @@ def Try():
     userID = request.form.get('id')
     return 'Nie Wentao'
 
-@app.route('/')
-def dir():
-    return render_template('dir.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=80) # 127.0.0.1:回路，自己访问自己
