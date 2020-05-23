d58087e834829a39dc255b52884cf584e53d2e90
161250020 <799544882@qq.com>
2020-05-23 17:08:12 +0800

    debug

--- Enterprise/controller/relationship.py
+++ Enterprise/controller/relationship.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from Enterprise.model.glo import Glo
+from Enterprise.service.get_data import GetData
 from Enterprise.service.interpersonal_network import InterpersonalNetwork
 
 relationship = Blueprint('relationship', __name__)
@@ -8,9 +9,10 @@ relationship = Blueprint('relationship', __name__)
 @relationship.route('/')
 def choose():
     # 人际关系网络
+    GetData.load_data()
     interpersonal_network = InterpersonalNetwork()
     for i in range(0, len(Glo.from_email)):
-        interpersonal_network.add_node(interpersonal_network, Glo.from_email[i], Glo.to_email[i])
+        interpersonal_network.add_node(Glo.from_email[i], Glo.to_email[i])
     node = []  # node的值要乘以5，以免太小了显示不出来
     link = []
     outDegree = interpersonal_network.outDegree  # 由于不显示有向图，进这个属性就足够了
